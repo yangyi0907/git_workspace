c72f59c5bf1009b76a770b37213af9fc269b5fcf
yangyi0907 <yangyi0907@mail.ustc.edu.cn>
2021-07-28 08:19:38 +0800

    分

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -7,7 +7,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
     }
 }
