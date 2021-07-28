d3166b6a43ae4b4129aacbc9f7ad16ba2d05f07b
yangyi0907 <yangyi0907@mail.ustc.edu.cn>
2021-07-28 08:33:01 +0800

    主干添加

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -11,5 +11,7 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("创建分支");
+
+        System.out.println("主干添加");
     }
 }
